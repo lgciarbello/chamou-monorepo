d951b86944088e5585f3252bbbeb01e6128cf36c
luisciarbello <lg10_ciarbello@hotmail.com>
2024-08-04 22:43:00 -0300

    generic-modal: adição de lógica para visualização do "default-button" ou "banner-preco-total"

--- src/app/generic-modal/generic-modal.component.ts
+++ src/app/generic-modal/generic-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-generic-modal',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './generic-modal.component.scss'
 })
 export class GenericModalComponent {
+  @Input() hasButton: boolean = false;
 
 }
