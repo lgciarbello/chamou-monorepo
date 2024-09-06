de259d164d4417dae8555e5cb546ffa2c8c5f60c
luisciarbello <lg10_ciarbello@hotmail.com>
2024-09-06 18:32:40 -0300

    cart-item: definição de funções de exclusão e edição de item;

--- projects/chamou/src/app/components/cart-item/cart-item.component.ts
+++ projects/chamou/src/app/components/cart-item/cart-item.component.ts
@@ -1,4 +1,7 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {ModalService} from "../../services/modal.service";
+import {ItemModalInput} from "../../interfaces/modal/item-modal-input.interface";
+import {AlertModalInput} from "../../interfaces/modal/alert-modal-input.interface";
 
 @Component({
   selector: 'app-cart-item',
@@ -6,11 +9,58 @@ import {Component, Input} from '@angular/core';
   styleUrl: './cart-item.component.scss'
 })
 export class CartItemComponent {
+  @Input({required: true}) public id: string = '';
   @Input({required: true}) public titulo: string = '';
   @Input({required: true}) public preco: number = 0.00;
   @Input({required: true}) public quantidade: number = 0.00;
+  @Input({required: true}) descricao: string = '';
+  @Input({required: true}) customizacoes: string[] = [];
   @Input() public informacoes: string = '';
   @Input() public linkImagem: string = "/assets/img/default_banner.png";
 
+  @Output() public excluirItem = new EventEmitter<void>();
+  @Output() public editarItem = new EventEmitter<ItemModalInput>();
 
+  constructor(private readonly _modalService: ModalService) {
+  }
+
+  onEdit() {
+    const data: ItemModalInput = {
+      customizacoes: this.customizacoes,
+      descricao: this.descricao,
+      id: this.id,
+      imagePath: this.linkImagem,
+      preco: this.preco,
+      quantidade: this.quantidade,
+      titulo: this.titulo
+    } as ItemModalInput;
+
+    this._modalService.openItemModal(data)
+      .afterClosed().subscribe(
+      itemModalInput => {
+        if (itemModalInput && itemModalInput.quantidade > 0) {
+          this.editarItem.emit(itemModalInput);
+        }
+      }
+    );
+  }
+
+  onDelete() {
+    const data: AlertModalInput = {
+      title: 'Excluir Item',
+      icon: 'trash-can',
+      message: 'Deseja mesmo excluir este item?',
+      buttonAcceptText: 'Excluir',
+      buttonDeclineText: 'Cancelar'
+    } as AlertModalInput;
+
+    this._modalService.openAlertModal(data)
+      .afterClosed().subscribe(
+        isAccept => {
+          if (isAccept) {
+            this.excluirItem.emit();
+          }
+        }
+    );
+  }
 }
