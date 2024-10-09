17535c597f07a95caade578818df42e75f59e77d
luisciarbello <lg10_ciarbello@hotmail.com>
2024-10-09 07:50:03 -0300

    portal-admin: - itens-page: implementação de lógica para redirecionamento à tela de criação de item;

--- projects/admin/src/app/components/pages/itens/itens-page/itens-page.component.ts
+++ projects/admin/src/app/components/pages/itens/itens-page/itens-page.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {IconProp} from "@fortawesome/fontawesome-svg-core";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-itens-page',
@@ -9,12 +10,18 @@ import {IconProp} from "@fortawesome/fontawesome-svg-core";
 export class ItensPageComponent implements OnInit {
   actionButtons: Map<string, IconProp> = new Map();
 
+  constructor(private readonly router: Router,
+              private readonly route: ActivatedRoute,) {
+  }
+
   ngOnInit() {
     this.actionButtons.set("adicionar", "plus");
     this.actionButtons.set("recarregar", "arrow-rotate-left")
   }
 
   buttonClicked(event: string) {
-    console.log(event);
+    if (event === 'adicionar') {
+      this.router.navigate(["item"], { relativeTo: this.route});
+    }
   }
 }
