ae73ccbea7832ea642a6d2d5de1a400df8551b34
luisciarbello <lg10_ciarbello@hotmail.com>
2024-11-28 15:08:24 -0300

    atualização conter o path do backend (koyeb);

--- projects/chamou/src/environments/environment.ts
+++ projects/chamou/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: 'http://chamou-server.chamou-app.internal:8080/chamou',
+  apiUrl: 'http://chamou-server.chamou-app.internal:8080',
   firebase: {
     apiKey: "AIzaSyAa5WgW1wCV_9PkKS7HyIQcdolpjqK82ao",
     authDomain: "chamou-amigao.firebaseapp.com",
